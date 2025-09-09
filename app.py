564e14ee99ec8d6298d9a29fd6abea703e96abdd
Git User <constantlycooking@users.noreply.github.com>
2025-09-09 14:14:36 +0200

    V8 | added qwen edit | 09/09/25, 14:14:36

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 import torch
-from diffusers import StableDiffusionXLPipeline, DiffusionPipeline, FlowMatchEulerDiscreteScheduler
+from diffusers import StableDiffusionXLPipeline, DiffusionPipeline, FlowMatchEulerDiscreteScheduler, QwenImageEditPipeline
 import math
 import os
 
@@ -17,31 +17,40 @@ def get_pipeline(model_name):
     if current_model_name == model_name and pipeline_cache is not None:
         return pipeline_cache
 
+    # Common scheduler for Qwen models
+    scheduler_config = {
+        "base_image_seq_len": 256,
+        "base_shift": math.log(3),
+        "invert_sigmas": False,
+        "max_image_seq_len": 8192,
+        "max_shift": math.log(3),
+        "num_train_timesteps": 1000,
+        "shift": 1.0,
+        "shift_terminal": None,
+        "stochastic_sampling": False,
+        "time_shift_type": "exponential",
+        "use_beta_sigmas": False,
+        "use_dynamic_shifting": True,
+        "use_exponential_sigmas": False,
+        "use_karras_sigmas": False,
+    }
+    scheduler = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
+
     if model_name == "epicrealism":
         pipeline = StableDiffusionXLPipeline.from_pretrained("glides/epicrealismxl", cache_dir=CACHE_DIR).to("cuda")
     elif model_name == "qwen":
-        scheduler_config = {
-            "base_image_seq_len": 256,
-            "base_shift": math.log(3),
-            "invert_sigmas": False,
-            "max_image_seq_len": 8192,
-            "max_shift": math.log(3),
-            "num_train_timesteps": 1000,
-            "shift": 1.0,
-            "shift_terminal": None,
-            "stochastic_sampling": False,
-            "time_shift_type": "exponential",
-            "use_beta_sigmas": False,
-            "use_dynamic_shifting": True,
-            "use_exponential_sigmas": False,
-            "use_karras_sigmas": False,
-        }
-        scheduler = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
         pipeline = DiffusionPipeline.from_pretrained(
             "Qwen/Qwen-Image", scheduler=scheduler, torch_dtype=torch.bfloat16, cache_dir=CACHE_DIR
         ).to("cuda")
         pipeline.load_lora_weights(
             "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Lightning-8steps-V1.0.safetensors", cache_dir=CACHE_DIR
+        )
+    elif model_name == "qwen-edit":
+        pipeline = QwenImageEditPipeline.from_pretrained(
+            "Qwen/Qwen-Image-Edit", scheduler=scheduler, torch_dtype=torch.bfloat16, cache_dir=CACHE_DIR
+        ).to("cuda")
+        pipeline.load_lora_weights(
+            "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Edit-Lightning-8steps-V1.0.safetensors", cache_dir=CACHE_DIR
         )
     else:
         raise ValueError("Unknown model name")
@@ -57,6 +66,12 @@ def generate_epicrealism(prompt, negative_prompt, width, height, sample_steps):
 def generate_qwen(prompt, negative_prompt, width, height, sample_steps):
     pipeline = get_pipeline("qwen")
     return pipeline(prompt=prompt, negative_prompt=negative_prompt, width=width, height=height, num_inference_steps=sample_steps, true_cfg_scale=1.0).images[0]
+
+def generate_qwen_edit(image, prompt, negative_prompt, sample_steps):
+    if image is None:
+        raise gr.Error("Please upload an image to edit.")
+    pipeline = get_pipeline("qwen-edit")
+    return pipeline(image=image, prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=sample_steps, true_cfg_scale=4.0, generator=torch.manual_seed(0)).images[0]
 
 
 with gr.Blocks() as interface:
@@ -98,6 +113,19 @@ with gr.Blocks() as interface:
                             with gr.Column():
                                 sampling_steps_qwen = gr.Slider(label="Sampling Steps", info="The number of denoising steps.", value=8, minimum=1, maximum=20, step=1, interactive=True)
             generate_button_qwen.click(fn=generate_qwen, inputs=[prompt_qwen, negative_prompt_qwen, width_qwen, height_qwen, sampling_steps_qwen], outputs=[output_qwen])
+
+        with gr.TabItem("qwen-image-edit + lightning"):
+            with gr.Row():
+                with gr.Column():
+                    input_image_edit = gr.Image(type="pil", label="Input Image")
+                    prompt_edit = gr.Textbox(label="Prompt", info="What do you want to change?", value="Change the rabbit's color to purple, with a flash light background.", lines=3, interactive=True)
+                    negative_prompt_edit = gr.Textbox(label="Negative Prompt", info="What do you want to exclude from the image?", value=" ", lines=3, interactive=True)
+                    sampling_steps_edit = gr.Slider(label="Sampling Steps", info="The number of denoising steps.", value=8, minimum=1, maximum=20, step=1, interactive=True)
+                    generate_button_edit = gr.Button("Generate")
+                with gr.Column():
+                    output_edit = gr.Image()
+            generate_button_edit.click(fn=generate_qwen_edit, inputs=[input_image_edit, prompt_edit, negative_prompt_edit, sampling_steps_edit], outputs=[output_edit])
+
 
 if __name__ == "__main__":
     interface.launch(share=True)
